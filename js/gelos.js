ec55963a32cfe557ed8d37744cbaa9ec967fc048
tulip-git <laleh.mohammadi@gmail.com>
2024-04-05 21:47:31 +1100

    search

--- js/gelos.js
+++ js/gelos.js
@@ -2,7 +2,7 @@
 
   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
   function responsiveNav() {
-      var x = document.getElementById("myTopnav");
+      var x = document.getElementById("myTopNav");
       if (x.className === "topnav") {
           x.className += " responsive";
       } else {
